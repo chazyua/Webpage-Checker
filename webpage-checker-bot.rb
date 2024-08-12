426e279eec544c35cf2519b463f99af79e024fd5
chazyua <art.t35t@gmail.com>
2024-08-11 20:12:38 -0700

    added support for multiple webpage pages

--- webpage-checker-bot.rb
+++ webpage-checker-bot.rb
@@ -5,7 +5,7 @@ require 'telegram/bot'
 require 'dotenv/load'
 
 # Configuration
-URL = 'https://www.google.com'
+URLS = ['https://www.google.com', 'https://www.example.com', 'https://www.anotherexample.com']
 HASH_FILE = './last_hash.txt'
 TELEGRAM_TOKEN = ENV['TELEGRAM_TOKEN']
 CHAT_ID = ENV['CHAT_ID']
@@ -36,13 +36,15 @@ def send_telegram_message(token, chat_id, message)
 end
 
 def main
-  content = get_website_content(URL)
-  current_hash = calculate_hash(content)
-  last_hash = read_last_hash(HASH_FILE)
+  URLS.each do |url|
+    content = get_website_content(url)
+    current_hash = calculate_hash(content)
+    last_hash = read_last_hash("#{HASH_FILE}_#{url}")
 
-  if current_hash != last_hash
-    send_telegram_message(TELEGRAM_TOKEN, CHAT_ID, "The content of #{URL} has changed.")
-    write_current_hash(HASH_FILE, current_hash)
+    if current_hash != last_hash
+      send_telegram_message(TELEGRAM_TOKEN, CHAT_ID, "The content of #{url} has changed.")
+      write_current_hash("#{HASH_FILE}_#{url}", current_hash)
+    end
   end
 end
 
