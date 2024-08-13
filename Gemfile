b414a54c73871589a6675df1858263c70b0c9cb8
chazyua <art.t35t@gmail.com>
2024-08-12 22:36:38 -0700

    few more updates

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'net-http'
+gem 'digest'
+gem 'fileutils'
+gem 'telegram-bot-ruby'
+gem 'dotenv'
--- webpage-checker-bot.rb
+++ webpage-checker-bot.rb
@@ -5,8 +5,8 @@ require 'telegram/bot'
 require 'dotenv/load'
 
 # Configuration
-URLS = ['https://www.google.com', 'https://www.example.com', 'https://www.anotherexample.com']
-HASH_FILE = './last_hash.txt'
+URLS = ['https://bing.com', 'https://google.com']
+HASH_FILE = './hashes/last_hash.txt'
 TELEGRAM_TOKEN = ENV['TELEGRAM_TOKEN']
 CHAT_ID = ENV['CHAT_ID']
 
@@ -36,14 +36,17 @@ def send_telegram_message(token, chat_id, message)
 end
 
 def main
+  FileUtils.mkdir_p('hashes') unless File.directory?('hashes')
+
   URLS.each do |url|
     content = get_website_content(url)
     current_hash = calculate_hash(content)
-    last_hash = read_last_hash("#{HASH_FILE}_#{url}")
+    file_path = "./hashes/#{url.gsub('https://', '')}_last_hash.txt"
+    last_hash = read_last_hash(file_path)
 
     if current_hash != last_hash
       send_telegram_message(TELEGRAM_TOKEN, CHAT_ID, "The content of #{url} has changed.")
-      write_current_hash("#{HASH_FILE}_#{url}", current_hash)
+      write_current_hash(file_path, current_hash)
     end
   end
 end
